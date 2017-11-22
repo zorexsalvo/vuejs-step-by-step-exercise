7926167e93a094af69689a1bd017cc19d76e5297
Zorex Salvo <zorexsalvo@gmail.com>
2017-11-22 14:23:13 +0800

    Add the router-links to App.vue

--- vuez/src/App.vue
+++ vuez/src/App.vue
@@ -1,5 +1,16 @@
 <template>
   <div id="app">
+    <div class="container">
+        <nav class="navbar" role="navigation" aria-label="main navigation">
+            <div class="navbar-brand">
+                <a class="navbar-item" href="">
+                    <img src="./assets/images/vuez-logo.png" alt="">
+                </a>
+                <router-link class="navbar-item" to="/">Home</router-link>
+            </div>
+        </nav>
+    </div>
+
     <router-view/>
   </div>
 </template>
