63a3a0fcf8f0e5839d812625f8fe14ba7d4f88dd
Zorex Salvo <zorexsalvo@gmail.com>
2017-11-22 14:19:13 +0800

    Create pages directory and add HomePage

---
+++ vuez/src/pages/Home.vue
@@ -0,0 +1,12 @@
+<template>
+    <div>
+        <section class="hero is-medium is-primary is-bold">
+            <div class="hero-body">
+                <div class="container">
+                    <h1 class="title">Home Page</h1>
+                    <h2 class="subtitle">Description Here</h2>
+                </div>
+            </div>
+        </section>
+    </div>
+</template>
