761924a9e8aef2c641be2176da7454c5b0e1e8bf
Zorex Salvo <zorexsalvo@gmail.com>
2017-11-23 14:04:00 +0800

    Design the site for my blog site

--- vuez/src/App.vue
+++ vuez/src/App.vue
@@ -1,17 +1,28 @@
 <template>
   <div id="app">
     <div class="container">
-        <nav class="navbar" role="navigation" aria-label="main navigation">
-            <div class="navbar-brand">
-                <a class="navbar-item" href="">
-                    <img src="./assets/images/vuez-logo.png" alt="">
-                </a>
-                <router-link class="navbar-item" to="/">Home</router-link>
+        <nav class="navbar is-transparent" role="navigation" aria-label="main navigation">
+            <div class="navbar-end">
+                <a class="navbar-item" href="http://github.com/zorexsalvo"><span class="icon"><i class="fa fa-lg fa-github"></i></span></a>
+                <a class="navbar-item" href="http://github.com/zorexsalvo"><span class="icon"><i class="fa fa-lg fa-twitter"></i></span></a>
+                <a class="navbar-item" href="http://github.com/zorexsalvo"><span class="icon"><i class="fa fa-lg fa-facebook"></i></span></a>
             </div>
         </nav>
     </div>
 
     <router-view/>
+
+    <br>
+    <footer class="footer">
+        <div class="container">
+            <div class="content has-text-centered">
+                <p>
+                A blog <strong>site</strong> by <a href="https://zorexsalvo.com">@zorexsalvo</a>.
+                </p>
+            </div>
+        </div>
+    </footer>
+
   </div>
 </template>
 
@@ -23,4 +34,5 @@ export default {
 
 <style lang="css">
   @import "../node_modules/bulma/css/bulma.css";
+  @import "https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css";
 </style>
--- vuez/src/pages/Home.vue
+++ vuez/src/pages/Home.vue
@@ -1,12 +1,155 @@
 <template>
     <div>
-        <section class="hero is-medium is-primary is-bold">
+        <section class="hero is-medium is-warning is-bold">
             <div class="hero-body">
                 <div class="container">
-                    <h1 class="title">Home Page</h1>
-                    <h2 class="subtitle">Description Here</h2>
+                    <h1 class="title">a blog by @zorexsalvo</h1>
+                    <h2 class="subtitle">web developer</h2>
                 </div>
             </div>
         </section>
+
+        <br><br>
+
+        <div class="container is-fluid" style="margin-left: 100px; margin-right: 100px;">
+            <div class="columns">
+
+                <div class="column is-two-fifths">
+                    <div class="card">
+                        <div class="card-image">
+                            <figure class="image is-4by4">
+                                <img src="https://zorexsalvo.com/img/AWS-Summit-2017.png" alt="">
+                            </figure>
+                        </div>
+                        <div class="card-content">
+                            <p class="title is-4">AWS Summit Manila 2017</p>
+                            <p class="subtitle is-6">@zorexsalvo</p>
+
+                            <div class="content">
+                            <p>It is a one day summit that showcases all the cool products developed and offered by Amazon Web Services.
+                            </p>
+                            </div>
+                        </div>
+                        <footer class="card-footer">
+                            <p class="card-footer-item">
+                            <span>
+                                View on <a href="https://twitter.com/codinghorror/status/506010907021828096">Twitter</a>
+                            </span>
+                            </p>
+                            <p class="card-footer-item">
+                            <span>
+                                Share on <a href="#">Facebook</a>
+                            </span>
+                            </p>
+                        </footer>
+                    </div>
+                </div>
+
+                <div class="column">
+
+                    <div class="box">
+                        <article class="media">
+                            <div class="media-left">
+                                <figure class="image is-64x64">
+                                    <img src="https://bulma.io/images/placeholders/128x128.png" alt="Image">
+                                </figure>
+                            </div>
+                            <div class="media-content">
+                                <div class="content">
+                                    <p>
+                                    <strong>John Smith</strong> <small>@johnsmith</small> <small>31m</small>
+                                    <br>
+                                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean efficitur sit amet massa fringilla egestas. Nullam condimentum luctus turpis.
+                                    </p>
+                                </div>
+                                <nav class="level is-mobile">
+                                    <div class="level-left">
+                                        <a class="level-item">
+                                            <span class="icon is-small"><i class="fa fa-reply"></i></span>
+                                        </a>
+                                        <a class="level-item">
+                                            <span class="icon is-small"><i class="fa fa-retweet"></i></span>
+                                        </a>
+                                        <a class="level-item">
+                                            <span class="icon is-small"><i class="fa fa-heart"></i></span>
+                                        </a>
+                                    </div>
+                                </nav>
+                            </div>
+                        </article>
+                    </div>
+
+                    <div class="box">
+                        <article class="media">
+                            <div class="media-left">
+                                <figure class="image is-64x64">
+                                    <img src="https://bulma.io/images/placeholders/128x128.png" alt="Image">
+                                </figure>
+                            </div>
+                            <div class="media-content">
+                                <div class="content">
+                                    <p>
+                                    <strong>John Smith</strong> <small>@johnsmith</small> <small>31m</small>
+                                    <br>
+                                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean efficitur sit amet massa fringilla egestas. Nullam condimentum luctus turpis.
+                                    </p>
+                                </div>
+                                <nav class="level is-mobile">
+                                    <div class="level-left">
+                                        <a class="level-item">
+                                            <span class="icon is-small"><i class="fa fa-reply"></i></span>
+                                        </a>
+                                        <a class="level-item">
+                                            <span class="icon is-small"><i class="fa fa-retweet"></i></span>
+                                        </a>
+                                        <a class="level-item">
+                                            <span class="icon is-small"><i class="fa fa-heart"></i></span>
+                                        </a>
+                                    </div>
+                                </nav>
+                            </div>
+                        </article>
+                    </div>
+
+                    <div class="box">
+                        <article class="media">
+                            <div class="media-left">
+                                <figure class="image is-128x128">
+                                    <img src="https://zorexsalvo.com/img/pet_project.jpg" alt="Image">
+                                </figure>
+                            </div>
+                            <div class="media-content">
+                                <div class="content">
+                                    <p>
+                                    <small>October 1, 2017</small> 
+                                    <br>
+                                    Pet Projects: How to stay motivated doing it?
+                                    </p>
+                                </div>
+                                <nav class="level is-mobile">
+                                    <div class="level-left">
+                                        <a class="level-item">
+                                            <span class="icon is-small"><i class="fa fa-reply"></i></span>
+                                        </a>
+                                        <a class="level-item">
+                                            <span class="icon is-small"><i class="fa fa-retweet"></i></span>
+                                        </a>
+                                        <a class="level-item">
+                                            <span class="icon is-small"><i class="fa fa-heart"></i></span>
+                                        </a>
+                                    </div>
+                                </nav>
+                            </div>
+                        </article>
+                    </div>
+
+
+
+
+                </div>
+
+            </div>
+        </div>
+
     </div>
 </template>
